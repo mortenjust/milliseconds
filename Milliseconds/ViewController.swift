56eea23e510d0d6179d846d5809d9137e0a2265a
mortenjust <morten.just@genstart.dk>
2014-11-10 22:36:01 -0800

    Initial Commit

---
+++ Milliseconds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Milliseconds
+//
+//  Created by Morten Just Petersen on 11/10/14.
+//  Copyright (c) 2014 Morten Just Petersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
